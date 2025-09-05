7297f5e60974109c14fc40ddcabfa10d2733a242
DESKTOP-B19SIA8\Admin <nguyendien2804@gmail.com>
2025-09-05 09:01:07 +0700

    1. generics

---
+++ 1.generics/Generic/Generic/Box.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    public class Box<T>
+    {
+        public required T Value { get; set; }
+    }
+
+    public class IntBox
+    {
+        public int Value { get; set; }
+    }
+    public class StringBox
+    {
+        public required string Value { get; set; }
+    }
+}
---
+++ 1.generics/Generic/Generic/Program.cs
@@ -0,0 +1,48 @@
+﻿//using Generic;
+
+//var intBox = new IntBox()
+//{
+//    Value = 1
+//};
+
+//var stringBox = new StringBox()
+//{
+//    Value = "string"
+//};
+
+//Console.WriteLine("int box value: " + intBox.Value);
+//Console.WriteLine("string box value: " + stringBox.Value);
+
+//var genericIntBox = new Box<int>()
+//{
+//    Value = 2
+//};
+
+//var genericStringBox = new Box<string>()
+//{
+//    Value = "generic string"
+//};
+
+//Console.WriteLine("generic int box value: " + genericIntBox.Value);
+//Console.WriteLine("generic string box value: " + genericStringBox.Value);
+
+
+// generic method
+static void Swap<T>(ref T a, ref T b)
+{
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
+// ex1:T is int
+int x = 5, y = 10;
+Console.WriteLine($"old value: x = {x}, y = {y}");
+Swap(ref x, ref y);
+Console.WriteLine($"new value: x = {x}, y = {y}");
+
+// ex2:T is string
+string first = "Alice", second = "Bob";
+Console.WriteLine($"old value: first  = {first}, second = {second}");
+Swap(ref first, ref second); // also works
+Console.WriteLine($"new value: first  = {first}, second = {second}");
